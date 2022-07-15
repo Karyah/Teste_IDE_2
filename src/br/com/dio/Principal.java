a77627a8efb971e02dc792f6ebb044d6cab048d1
Karyah <mastrogiacomokaren@gmail.com>
2022-07-15 11:11:22 -0300

    Commit.

--- src/br/com/dio/Principal.java
+++ src/br/com/dio/Principal.java
@@ -10,10 +10,15 @@ public class Principal {
 		Scanner ent = new Scanner(System.in);
 		Funcionario func = new Funcionario();
 		
+		System.out.println("digite o seu nome:");
+		func.nome = ent.next();
+		System.out.println("seu nome:"+func.nome);
+		
 		System.out.println("digite o cpf:");
 		func.cpf = ent.nextInt();
 		System.out.println("seu cpf:" + func.cpf);
 
+		
 	}
 	
 }
--- src/br/com/dio/model/Funcionario.java
+++ src/br/com/dio/model/Funcionario.java
@@ -3,6 +3,7 @@ package br.com.dio.model;
 public class Funcionario {
 	
 	public String nome;
+	public int telefone;
 	public int cpf;
 	private String senha;
 	
